b19c556943b8f76439e4f4e6a3003b699dc124d7
Rajkumar verma <rv782975@gmail.com>
2025-11-04 13:20:29 +0530

    change server .js

--- nodejs with flask/server.js
+++ nodejs with flask/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/index.js"; // ✅ FIXED import path
-
+import axios from "axios";
 dotenv.config();
 
 const app = express();
@@ -12,6 +12,22 @@ app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
+  async function getYoutubeKey() {
+    const res = await axios.get("https://www.youtube.com/watch?v=mwYsapR6cYk");
+    const html = res.data;
+    const versionMatch = html.match(
+      /"INNERTUBE_CONTEXT_CLIENT_VERSION":"([0-9\.]+)"/
+    );
+    const clientVersion = versionMatch ? versionMatch[1] : "2.20240214.01.00";
+
+    // Regex to extract key
+    const match = html.match(/"INNERTUBE_API_KEY":"([A-Za-z0-9_\-]+)"/);
+    if (match) return [match[1], versionMatch, clientVersion];
+    return null;
+  }
+
+  getYoutubeKey().then(console.log);
+  
   res.json({ message: "Main Node backend running" });
 });
 
